08040681a7bd5003a0770ca156958057d79ce24f
ohmono <basura1300@gmail.com>
2021-05-09 20:40:35 -0500

    add document 1 and exe

---
+++ src/ui.py
@@ -0,0 +1,110 @@
+from PyQt5 import QtCore, QtWidgets
+import webbrowser
+
+from lexic import analizator
+
+
+class Ui_Dialog(QtWidgets.QWidget):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.setWindowModality(QtCore.Qt.NonModal)
+        Dialog.setEnabled(True)
+        Dialog.resize(541, 431)
+        Dialog.setMinimumSize(QtCore.QSize(541, 431))
+        Dialog.setMaximumSize(QtCore.QSize(541, 431))
+        Dialog.setMouseTracking(False)
+        Dialog.setAutoFillBackground(False)
+        Dialog.setModal(False)
+        self.button_compile = QtWidgets.QPushButton(
+            Dialog, clicked=lambda: self.click_compile())
+        self.button_compile.setEnabled(False)
+        self.button_compile.setGeometry(QtCore.QRect(10, 390, 75, 23))
+        self.button_compile.setObjectName("button_compile")
+        self.text_code = QtWidgets.QTextEdit(Dialog)
+        self.text_code.setEnabled(False)
+        self.text_code.setGeometry(QtCore.QRect(10, 50, 261, 321))
+        self.text_code.setObjectName("text_code")
+        self.table_token = QtWidgets.QTableWidget(Dialog)
+        self.table_token.setEnabled(False)
+        self.table_token.setGeometry(QtCore.QRect(290, 20, 241, 351))
+        self.table_token.setObjectName("table_token")
+        self.table_token.setColumnCount(2)
+        self.table_token.setRowCount(0)
+        item = QtWidgets.QTableWidgetItem()
+        self.table_token.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.table_token.setHorizontalHeaderItem(1, item)
+        self.button_about = QtWidgets.QPushButton(
+            Dialog, clicked=lambda: webbrowser.open('https://github.com/ohmono/analizador-lexico'))
+        self.button_about.setGeometry(QtCore.QRect(444, 390, 91, 23))
+        self.button_about.setObjectName("button_about")
+        self.button_clear = QtWidgets.QPushButton(
+            Dialog, clicked=lambda: self.click_clear())
+        self.button_clear.setEnabled(False)
+        self.button_clear.setGeometry(QtCore.QRect(110, 390, 75, 23))
+        self.button_clear.setObjectName("button_clear")
+        self.button_upload = QtWidgets.QPushButton(
+            Dialog, clicked=lambda: self.click_upload())
+        self.button_upload.setGeometry(QtCore.QRect(10, 20, 91, 23))
+        self.button_upload.setObjectName("button_upload")
+        self.button_draw = QtWidgets.QPushButton(
+            Dialog, clicked=lambda: self.click_draw())
+        self.button_draw.setGeometry(QtCore.QRect(140, 20, 91, 23))
+        self.button_draw.setObjectName("button_draw")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Analizador Léxico"))
+        self.button_compile.setText(_translate("Dialog", "Compilar"))
+        item = self.table_token.horizontalHeaderItem(0)
+        item.setText(_translate("Dialog", "Lexema"))
+        item = self.table_token.horizontalHeaderItem(1)
+        item.setText(_translate("Dialog", "Token"))
+        self.button_about.setText(_translate("Dialog", "Documentación"))
+        self.button_clear.setText(_translate("Dialog", "Limpiar"))
+        self.button_upload.setText(_translate("Dialog", "Adjuntar archivo"))
+        self.button_draw.setText(_translate("Dialog", "Escribir código"))
+
+    def click_draw(self):
+        self.text_code.setEnabled(True)
+        self.button_upload.setEnabled(False)
+        self.button_compile.setEnabled(True)
+        self.button_draw.setEnabled(False)
+
+    def click_compile(self):
+        self.text_code.setEnabled(False)
+        self.table_token.setEnabled(True)
+        self.button_clear.setEnabled(True)
+        self.button_compile.setEnabled(False)
+        self.table_token.insertRow(0)
+        global sol
+        txt = self.text_code.toPlainText()
+        sol = analizator().token_list(txt.replace('\n', ' '))
+        self.table_token.setRowCount(len(sol))
+        for i in range(len(sol)):
+            self.table_token.setItem(
+                i, 0, QtWidgets.QTableWidgetItem(sol[i][1]))
+            self.table_token.setItem(
+                i, 1, QtWidgets.QTableWidgetItem(sol[i][0]))
+
+    def click_clear(self):
+        self.text_code.setText('')
+        for _ in range(len(sol)):
+            self.table_token.removeRow(0)
+        self.table_token.setEnabled(False)
+        self.button_upload.setEnabled(True)
+        self.button_draw.setEnabled(True)
+        self.button_clear.setEnabled(False)
+
+    def click_upload(self):
+        fname = QtWidgets.QFileDialog.getOpenFileName(
+            self, 'Open file', '.\\', "Txt files (*.txt)")
+        if fname[0] != '':
+            txt = str(open(fname[0], 'r').read()).replace('\n', ' ')
+            self.text_code.setText((txt))
+            self.button_upload.setEnabled(False)
+            self.button_draw.setEnabled(False)
+            self.button_compile.setEnabled(True)
